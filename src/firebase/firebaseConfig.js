b65fa92d282ce52e386a6a15145c0db02f9206c6
Alwin Simon <003alwin@gmail.com>
2023-08-15 11:38:31 +0530

    Added firebase config file

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+import firebase  from "firebase";
+
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyALbVuprt6UX4fFDStsovOnOzNbggkHImM",
+  authDomain: "olx-clone-ba71c.firebaseapp.com",
+  projectId: "olx-clone-ba71c",
+  storageBucket: "olx-clone-ba71c.appspot.com",
+  messagingSenderId: "363632890900",
+  appId: "1:363632890900:web:d51709c0ceb1bf83130301",
+  measurementId: "G-2WP2FG99YY"
+  
+};
